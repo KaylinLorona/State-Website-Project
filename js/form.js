8f30291792943d99488df3cd70778c8b6d950607
Kaylin Lorona <kaylinlorona10@gmail.com>
2023-11-06 19:53:03 -0600

    added js code for form email verification

--- js/form.js
+++ js/form.js
@@ -1,13 +1,13 @@
 
 function verify()
     {
-    if(document.getElementById("email1").value == document.getElementById("email2").value) {
+    if(document.getElementById("email1").value === document.getElementById("email2").value) {
     
-        alert("matched")
+        alert("Email address match")
     
     } else {
         document.getElementById("submit").setAttribute("disabled", "disabled");
-        alert("not matched")
+        alert("Email address do not match")
         }
     
     }
